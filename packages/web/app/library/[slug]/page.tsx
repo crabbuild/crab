43a289bbc12a929b753c06c3d66e754943af2bcc
forhappy <haipingf@gmail.com>
2026-08-29 09:01:52 -0700

    fix(web): center library diagrams

--- packages/web/app/library/[slug]/page.tsx
+++ packages/web/app/library/[slug]/page.tsx
@@ -133,7 +133,7 @@ export default async function LibraryGuidePage({
   return (
     <MarketingLayout>
       <StructuredData data={structuredData} />
-      <article className="relative border-b border-border min-[1400px]:[&_.wide-article-visual]:max-w-[54rem] min-[1400px]:[&_.wide-article-visual]:-translate-x-1/2">
+      <article className="relative border-b border-border min-[1400px]:[&_.wide-article-visual]:relative min-[1400px]:[&_.wide-article-visual]:left-1/2 min-[1400px]:[&_.wide-article-visual]:max-w-[54rem] min-[1400px]:[&_.wide-article-visual]:-translate-x-1/2">
         <section className="mx-auto max-w-3xl px-4 pt-4 pb-10 sm:px-6 lg:px-8 lg:pt-8">
           <nav className="mb-8">
             <Link
